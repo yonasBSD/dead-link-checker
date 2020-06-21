e6742a26209ad1b0a24181bc960def9a9f05b97c
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2020-06-21 15:47:23 +0200

    Print parent page in output

--- delic/link_checker.py
+++ delic/link_checker.py
@@ -7,6 +7,7 @@ from html.parser import HTMLParser
 from urllib.parse import urljoin
 
 import requests
+from pydantic import BaseModel
 
 LINK_TAGS = {
     'a': ['href'],
@@ -17,12 +18,19 @@ LINK_TAGS = {
 }
 
 
+class Link(BaseModel):
+    '''Represents a single link with its attributes'''
+    url: str
+    parent_url: str
+
+
 class DelicHTMLParser(HTMLParser):
-    def __init__(self, link_queue, checked_urls, base_url):
+    def __init__(self, link_queue, checked_urls, base_url, parent_url):
         super().__init__()
         self.link_queue = link_queue
         self.checked_urls = checked_urls
         self.base_url = base_url
+        self.parent_url = parent_url
 
     def handle_starttag(self, tag, attrs):
         if tag in LINK_TAGS:
@@ -37,7 +45,11 @@ class DelicHTMLParser(HTMLParser):
 
                 # Add url to queue
                 if cleaned_url.startswith(self.base_url) and cleaned_url not in self.checked_urls:
-                    self.link_queue.put(cleaned_url)
+                    new_link = Link(
+                        url=cleaned_url,
+                        parent_url=self.parent_url,
+                    )
+                    self.link_queue.put(new_link)
 
 
 def check_site(base_url, workers_count):
@@ -55,8 +67,8 @@ def check_site(base_url, workers_count):
     def check_link_worker():
         while True:
             link = link_queue.get()
-            if link not in checked_urls:
-                checked_urls.append(link)
+            if link.url not in checked_urls:
+                checked_urls.append(link.url)
                 check_link(link_queue,
                            checked_urls,
                            broken_urls,
@@ -69,7 +81,11 @@ def check_site(base_url, workers_count):
         threading.Thread(target=check_link_worker, daemon=True).start()
 
     # Queue base URL
-    link_queue.put(base_url)
+    base_link = Link(
+        url=base_url,
+        parent_url='',
+    )
+    link_queue.put(base_link)
 
     # Wait until the queue fully processed
     link_queue.join()
@@ -77,8 +93,8 @@ def check_site(base_url, workers_count):
     # Return results
     return {
         'summary': {
-            'links_checked': len(checked_urls),
-            'links_broken': len(broken_urls),
+            'urls_checked': len(checked_urls),
+            'urls_broken': len(broken_urls),
         },
         'details': {
             'broken': broken_urls
@@ -86,17 +102,19 @@ def check_site(base_url, workers_count):
     }
 
 
-def check_link(link_queue, checked_urls, broken_links, base_url, url):
+def check_link(link_queue, checked_urls, broken_links, base_url, link: Link):
     '''Check a single link'''
     # Create parser
-    parser = DelicHTMLParser(link_queue, checked_urls, base_url)
+    parser = DelicHTMLParser(link_queue, checked_urls,
+                             base_url, link.url)
 
     # Fetch header
-    logging.info('Checking URL: %s', url)
-    req = requests.head(url)
+    logging.info('Checking URL: %s', link.url)
+    req = requests.head(link.url)
     if req.status_code >= 400:
         report = {
-            'url': url,
+            'page': link.parent_url,
+            'broken_url': link.url,
             'status': req.status_code,
         }
         broken_links.append(report)
@@ -104,5 +122,5 @@ def check_link(link_queue, checked_urls, broken_links, base_url, url):
     # Link is HTML page
     # Fetch and parse page
     if req.headers['content-type'].startswith('text/html'):
-        req_html = requests.get(url)
+        req_html = requests.get(link.url)
         parser.feed(req_html.text)
