e9df1f109a5b984d316b053334a335250c13c438
Jens Willemsens <6514515+JenswBE@users.noreply.github.com>
2020-06-27 08:48:07 +0200

    Make config more robust

--- delic/config.py
+++ delic/config.py
@@ -1,5 +1,6 @@
 '''Module to handle the config file'''
 
+import logging
 from pathlib import Path
 from typing import Dict
 
@@ -14,7 +15,24 @@ def load_yaml_file(path: Path) -> Dict:
 
 def load_config_file(path: Path) -> Dict:
     '''Load YAML config file and set defaults'''
+    # Load config
     config = load_yaml_file(path)
+
+    # Set defaults
     config['verbose'] = config.get('verbose', False)
     config['workers_per_site'] = config.get('workers_per_site', 8)
+
+    # Validate verbose
+    if not isinstance(config['verbose'], bool):
+        logging.warning('Verbose is not a bool: "%s". Defaulted to True.',
+                        config['verbose'])
+        config['verbose'] = True
+
+    # Validate workers_per_site
+    if not isinstance(config['workers_per_site'], int) or config['workers_per_site'] < 1:
+        logging.warning('Invalid number of workers: "%s". Defaulted to 8 workers.',
+                        config['workers_per_site'])
+        config['workers_per_site'] = 8
+
+    # Return result
     return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 '''Unit tests for config'''
 
 import pytest
+import logging
 
 from delic import config
 
@@ -28,7 +29,7 @@ def test_load_yaml_file(tmp_path):
     assert result == expected
 
 
-def test_load_config_file(tmp_path):
+def test_load_valid_config_file(tmp_path):
     '''Should read a YAML file and update with defaults'''
     # Write test file
     yaml_content = '''test:
@@ -47,6 +48,44 @@ def test_load_config_file(tmp_path):
         },
         'verbose': False,
         'workers_per_site': 8,
+    }
+
+    # Assert result
+    assert result == expected
+
+
+@pytest.mark.parametrize('verbose_config, expected_verbose', [
+    (False, False),
+    (True, True),
+    ('test', True),
+    (0, True),
+    (1, True),
+])
+@pytest.mark.parametrize('worker_config, expected_workers', [
+    (-10, 8),
+    (-1, 8),
+    (0, 8),
+    (1, 1),
+    (4, 4),
+    ('A', 8),
+])
+def test_load_valid_config_file(worker_config, expected_workers, verbose_config, expected_verbose, tmp_path):
+    '''Should read a YAML file and update with defaults'''
+    # Write test file
+    yaml_content = f'''
+                    verbose: {verbose_config}
+                    workers_per_site: {worker_config}
+                    '''
+    yaml_file = tmp_path / "test-config.yml"
+    yaml_file.write_text(yaml_content)
+
+    # Try to load file
+    result = config.load_config_file(yaml_file)
+
+    # Expected result
+    expected = {
+        'verbose': expected_verbose,
+        'workers_per_site': expected_workers,
     }
 
     # Assert result
