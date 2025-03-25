da1a19092f5010c593bbe55ea5528b9e657dfd95
JenswBE <6514515+JenswBE@users.noreply.github.com>
2025-03-25 20:21:23 +0100

    Bump dependencies

--- lint.sh
+++ lint.sh
@@ -1,2 +1,4 @@
 # This script is intended to be sourced like "source lint.sh" or ". lint.sh".
-golangci-lint run --config <(curl --silent https://raw.githubusercontent.com/JenswBE/setup/main/programming_configs/golang/.golangci.yml)
+curl --silent https://raw.githubusercontent.com/JenswBE/setup/main/programming_configs/golang/.golangci.yml -o .golangci.yml
+golangci-lint run
+rm .golangci.yml
